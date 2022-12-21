32e944e4f07a598af4297cdac158c440c24c43cc
leovoon <voonlihhaur@gmail.com>
2022-12-21 10:20:36 +0800

    chore: dump .vercel

--- .vercel/output/static/_app/immutable/chunks/0-f0879bf6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-7469703b.js";export{t as component};
--- .vercel/output/static/_app/immutable/chunks/1-bb39a357.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-168ea032.js";export{t as component};
